17f563c344d277d5a52963f5f64322404b7b285b
Thomas Cokelaer <cokelaer@gmail.com>
2019-03-23 21:00:45 +0100

    Fix issue with chromosome names set to numbers only

--- sequana/scripts/coverage.py
+++ sequana/scripts/coverage.py
@@ -418,8 +418,11 @@ def run_analysis(chrom, options, feature_dict):
 
     # Create directory and save ROIs
     directory = options.output_directory
+
     directory += os.sep + "coverage_reports"
-    directory += os.sep + chrom.chrom_name
+
+
+    directory += "{}{}".format(os.sep, chrom.chrom_name)
     mkdirs(directory)
     ROIs.df.to_csv("{}/rois.csv".format(directory))
 
