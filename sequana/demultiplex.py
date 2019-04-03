be1bfeed941b26294de5f0ed3a3e0d3a713192d1
Thomas Cokelaer <cokelaer@gmail.com>
2019-04-03 18:27:53 +0200

    Fix demultiplexage plots when only undetermined are found

--- sequana/demultiplex.py
+++ sequana/demultiplex.py
@@ -58,16 +58,29 @@ class StatsFile(object):
         for i, lane in enumerate(self.data["ConversionResults"]):
             total = 0
             for this in lane['DemuxResults']:
-                lanes.append(i+1)
+                lanes.append(lane['LaneNumber'])
                 names.append(this['SampleId'])
                 reads.append(this['NumberReads'])
                 total += this['NumberReads']
-            try:
-                lanes.append(i+1)
+
+            # if only undetermined (no sample sheet), no data should be found
+            # meaning that there is 0 determined and no names associated
+            # so we call it determined and store 0
+            if total == 0:
+                assert total ==0
+                names.append("Determined")
+                reads.append(0)
+                lanes.append(lane["LaneNumber"])
+
+            if "Undetermined" in lane:
                 names.append("Undetermined")
+                lanes.append(i+1)
                 reads.append(lane['Undetermined']['NumberReads'])
-            except:
-                pass
+            else:
+                names.append("Undetermined")
+                lanes.append(i+1)
+                reads.append(0)
+        print(lanes, names, reads)
 
         df = pd.DataFrame({"lane": lanes, "name": names, "count": reads})
         return df
@@ -86,31 +99,39 @@ class StatsFile(object):
 
             query = "lane==@lane and name=='Undetermined'"
             under = df.query(query)['count'].sum()
-            pylab.bar(range(L), counts, color="b", label="reads")
+            if total >0:
+                pylab.bar(range(L), counts, color="b", label="reads")
 
-            if 100 * under / total <20:
-                color = "green"
-            elif 100 * under / total <50:
-                color = "orange"
-            else:
+            if total == 0:
                 color = "red"
+            else:
+                if 100 * under / total <20:
+                    color = "green"
+                elif 100 * under / total <50:
+                    color = "orange"
+                else:
+                    color = "red"
 
             pylab.bar(range(L,L+1), under, color=color, label="undetermined")
             pylab.xticks([])
             pylab.ylabel("Number of reads")
-            pylab.legend()
+            try:pylab.legend()
+            except:pass
             pylab.title("Lane {}".format(lane))
             pylab.savefig(filename.format(lane), dpi=200)
 
     def barplot_summary(self, filename=None):
         df = self.get_data_reads()
         under = df.query("name=='Undetermined'")
+
         total = df.query("name!='Undetermined'")
         total = total.groupby("lane").sum().reset_index()
-        total["name"] = "Total"
+        print(total)
+        total["name"] = "Determined"
+
         df = pd.concat([under, total])
         df = df.pivot(index="lane", columns="name", values="count")
-        df = df[["Total", "Undetermined"]]
+        df = df[["Determined", "Undetermined"]]
         df.plot.barh(stacked=True)
         pylab.legend()
         pylab.xlabel("Number of reads")
@@ -122,9 +143,16 @@ class StatsFile(object):
     def plot_unknown_barcodes(self, N=20):
         ub = self.data['UnknownBarcodes']
         df = pd.DataFrame({x['Lane']:x['Barcodes'] for x in ub})
+        if "unknown" in df.index and len(df) == 1:
+            df.loc['known'] = [0 for i in df.columns]
+
+        # if data is made of undetermined only, the dataframe is just made of
+        # N lanes with one entry : unknown
         S = df.sum(axis=1).sort_values(ascending=False).index[0:N]
         data = df.loc[S][::-1]
+        print(data)
 
+        data.columns = ["Lane {}".format(x) for x in data.columns]
         from matplotlib import rcParams
         rcParams['axes.axisbelow'] = True
         pylab.figure(figsize=(10,8))
@@ -132,9 +160,9 @@ class StatsFile(object):
         data.plot(kind="barh", width=1, ec="k", ax=ax)
         rcParams['axes.axisbelow'] = False
         pylab.xlabel("Number of reads", fontsize=12)
-        pylab.ylabel("Frequency (%)", fontsize=12)  
+        pylab.ylabel("Frequency (%)", fontsize=12)
         pylab.grid(True)
-        pylab.legend(["Lane {}".format(x) for x in range(1, len(df)+1)])
+        pylab.legend(["Lane {}".format(x) for x in range(1, len(df.columns)+1)])
         try:
             pylab.tight_layout()
         except:
