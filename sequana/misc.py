be7e0781a81aab7f254d2d7b8405b5c87132dd3b
Thomas Cokelaer <cokelaer@gmail.com>
2019-11-25 16:57:45 +0100

    add common tools for pipelines

--- sequana/misc.py
+++ sequana/misc.py
@@ -29,7 +29,43 @@ logger.name = __name__
 from docutils import core
 from docutils.writers.html4css1 import Writer,HTMLTranslator
 
-__all__ = ['textwrap', 'rest2html', 'wget', 'findpos', 'on_cluster', "normpdf"]
+__all__ = ['textwrap', 'rest2html', 'wget', 'findpos', 'on_cluster', 
+    "normpdf", "Colors"]
+
+
+class Colors:
+    PURPLE = "\033[95m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+    def failed(self, msg):
+        return self.FAIL + msg + self.ENDC
+
+    def bold(self, msg):
+        return self.BOLD + msg + self.ENDC
+
+    def purple(self, msg):
+        return self.PURPLE + msg + self.ENDC
+
+    def underlined(self, msg):
+        return self.UNDERLINE + msg + self.ENDC
+
+    def fail(self, msg):
+        return self.FAIL + msg + self.ENDC
+
+    def warning(self, msg):
+        return self.WARNING + msg + self.ENDC
+
+    def green(self, msg):
+        return self.GREEN + msg + self.ENDC
+
+    def blue(self, msg):
+        return self.BLUE + msg + self.ENDC
 
 
 def normpdf(x, mu, sigma):
