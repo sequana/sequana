005a77a64b34b23c0fe98ef2c9b70a879cd6388c
Thomas Cokelaer <cokelaer@gmail.com>
2020-04-16 10:13:52 +0200

    Fix deprecated warning

--- doc/sphinxext/sphinx_gallery/gen_gallery.py
+++ doc/sphinxext/sphinx_gallery/gen_gallery.py
@@ -116,7 +116,7 @@ def setup(app):
     app.add_config_value('plot_gallery', True, 'html')
     app.add_config_value('abort_on_example_error', False, 'html')
     app.add_config_value('sphinx_gallery_conf', gallery_conf, 'html')
-    app.add_stylesheet('gallery.css')
+    app.app.add_css_file('gallery.css')
 
     app.connect('builder-inited', generate_gallery_rst)
 
