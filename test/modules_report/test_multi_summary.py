e10c042143efd319b5a37acd066e227d1c199d1f
Thomas Cokelaer <cokelaer@gmail.com>
2020-04-17 10:57:37 +0200

    add new test

---
+++ test/modules_report/test_multi_summary.py
@@ -0,0 +1,10 @@
+from sequana.modules_report import multi_summary
+
+
+def test_multi_summary():
+
+
+    ms = multi_summary.MultiSummary("summary_qc.json")                                           
+    ms.get_trimming_percent()                                                               
+    ms.create_report_content()                                                              
+
