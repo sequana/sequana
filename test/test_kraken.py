4b28a1b8b92f3b3d463f610652087f1dc0db63d2
Thomas Cokelaer <cokelaer@gmail.com>
2020-05-03 22:18:14 +0200

    update tests

--- test/test_kraken.py
+++ test/test_kraken.py
@@ -14,7 +14,7 @@ try:
 except:
     pass
 
-@pytest.mark.xfail
+#@pytest.mark.xfail
 def test_run_kraken_taxon():
 
     database = sequana_config_path + os.sep + "kraken_toydb"
@@ -23,27 +23,30 @@ def test_run_kraken_taxon():
 
     file1 = sequana_data("Hm2_GTGAAA_L005_R1_001.fastq.gz", "data")
     file2 = sequana_data("Hm2_GTGAAA_L005_R2_001.fastq.gz", "data")
-
     kt = KrakenAnalysis([file1, file2], database=database, threads=1)
     kt.run()
 
-    kt = KrakenAnalysis(file2, database=database, threads=1)
-    kt.run()
 
+def test_kraken_sequential():
+    database = sequana_config_path + os.sep + "kraken_toydb"
     p = tempfile.TemporaryDirectory()
 
-    kt = KrakenHierarchical([file1, file2], [database, database],
+    file1 = sequana_data("Hm2_GTGAAA_L005_R1_001.fastq.gz", "data")
+    file2 = sequana_data("Hm2_GTGAAA_L005_R2_001.fastq.gz", "data")
+
+    # Test paired data with 2 kraken1 DB
+    kt = KrakenSequential([file1, file2], [database, database],
             output_directory=p.name, force=True)
     kt.run()
 
-    kt = KrakenHierarchical(file1, [database, database],
+    kt = KrakenSequential(file1, [database, database],
         output_directory=p.name, force=True)
     kt.run()
 
     p.cleanup()
 
 
-@pytest.mark.xfail
+#@pytest.mark.xfail
 def test_kraken_results():
     test_file = sequana_data("test_kraken.out", "testing")
     k = KrakenResults(test_file)
@@ -62,7 +65,7 @@ def test_kraken_results():
         k.kraken_to_krona(fout.name )
         k.to_js(fout.name)
 
-@pytest.mark.xfail
+#@pytest.mark.xfail
 def test_kraken_pipeline():
     
     from sequana import KrakenPipeline
@@ -74,7 +77,7 @@ def test_kraken_pipeline():
 
 
 
-@pytest.mark.xfail
+#@pytest.mark.xfail
 def test_mkr():
     from sequana.kraken import MultiKrakenResults
     mkr = MultiKrakenResults([sequana_data("test_kraken_multiple_1.csv"),
