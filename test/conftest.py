56bce07f46314e40a3dcf46d5e2cfd7ce1aa9181
Thomas Cokelaer <cokelaer@gmail.com>
2020-05-04 10:23:11 +0200

    fix path to taxonomy db on travis

--- test/conftest.py
+++ test/conftest.py
@@ -7,12 +7,13 @@ skiptravis = pytest.mark.skipif("TRAVIS_PYTHON_VERSION" in os.environ,
 
 
 def pytest_runtest_setup(item):
-    print("hello")
     if "TRAVIS_PYTHON_VERSION" in os.environ or 1 == 1:
+        print("downloading toydb data from github")
         from sequana.taxonomy import NCBITaxonomy
         from sequana import sequana_config_path
+        #HOME = os.getenv('HOME')
         n = NCBITaxonomy(
             "https://raw.githubusercontent.com/sequana/data/master/kraken_toydb/taxonomy/names.dmp",
             "https://raw.githubusercontent.com/sequana/data/master/kraken_toydb/taxonomy/nodes.dmp")
-        n.create_taxonomy_file(sequana_config_path + os.sep + "taxonomy_test.dat")
+        n.create_taxonomy_file(sequana_config_path + os.sep + "taxonomy.dat")
 
