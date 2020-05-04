e8d233c02e3efaa03f0ba4f873adbe5a27577a87
Etienne Kornobis <ekornobis@gmail.com>
2020-05-04 17:42:55 +0200

    Add stub test and test data

--- test/test_rnadiff.py
+++ test/test_rnadiff.py
@@ -2,11 +2,12 @@ from sequana.rnadiff import RNADiffResults, plot_venn
 from sequana import sequana_data
 
 
+RNADIFF_DIR = sequana_data("rnadiff") + "/rnadiff_analysis_1"
+
 
 def test_rnadiff():
-    filename = sequana_data("test_complete.xlsx")
-    directory = ...
-    RNADiffResults(directory)
+    RNADiffResults(RNADIFF_DIR)
+
 
 def test_plot_venn():
-    plot_venn((100,10,200))
+    plot_venn(compa_list=[{"a", "b"}, {"a"}], labels=["t1", "t2"])
