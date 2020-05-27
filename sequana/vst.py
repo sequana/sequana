83c3ae86d8b7ec268d05607e729a72ed6f479094
Thomas Cokelaer <cokelaer@gmail.com>
2020-05-27 12:33:34 +0200

    more doc + license

--- sequana/vst.py
+++ sequana/vst.py
@@ -1,14 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+#  This file is part of Sequana software
+#
+#  Copyright (c) 2016-2020 - Sequana Development Team
+#
+#  File author(s):
+#      Thomas Cokelaer <thomas.cokelaer@pasteur.fr>
+#
+#  Distributed under the terms of the 3-clause BSD license.
+#  The full license is in the LICENSE file, distributed with this software.
+#
+#  website: https://github.com/sequana/sequana
+#  documentation: http://sequana.readthedocs.io
+#
+##############################################################################
+"""Simple VST transformation"""
 from sequana.lazy import numpy as np
 from sequana import logger
 
+__all__ = ['VST']
 
 class VST():
-
     """
 
+    ::
+    
+        v = VST()
+        v.anscombe(X)
+
     Reference: M. Makitalo and A. Foi, "Optimal inversion of the
-    #    generalized Anscombe transformation for Poisson-Gaussian noise",
-    #    IEEE Trans. Image Process., doi:10.1109/TIP.2012.2202675
+        generalized Anscombe transformation for Poisson-Gaussian noise",
+        IEEE Trans. Image Process., doi:10.1109/TIP.2012.2202675
 
     """
     def __init__(self):
