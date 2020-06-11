5ac971dc433ef0d48f3f1c1888bee09a2859be87
Thomas Cokelaer <cokelaer@gmail.com>
2020-06-11 17:49:55 +0200

    add ability to add TOC

---
+++ sequana/resources/js/jquery.tocPlugin.js
@@ -0,0 +1,26 @@
+jQuery.tableOfContents =
+function (tocList) {
+    jQuery(tocList).empty();
+    var prevH2Item = null;
+    var prevH2List = null;
+
+    var index = 0;
+    jQuery("h2, h3").each(function() {
+
+        var anchor = "<a name='" + index + "'></a>";
+        jQuery(this).before(anchor);
+
+        var li     = "<li><a href='#" + index + "'>" +
+                     jQuery(this).text() + "</a></li>";
+
+        if( jQuery(this).is("h2") ){
+            prevH2List = jQuery("<ul></ul>");
+            prevH2Item = jQuery(li);
+            prevH2Item.append(prevH2List);
+            prevH2Item.appendTo(tocList);
+        } else {
+            prevH2List.append(li);
+        }
+        index++;
+    });
+}
