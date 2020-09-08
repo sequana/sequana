ede6c600f5e01521cba0e7d671878780d1178d86
Thomas Cokelaer <cokelaer@gmail.com>
2020-09-08 23:15:27 +0200

    add new module

---
+++ sequana/protein.py
@@ -0,0 +1,36 @@
+import re
+import string
+import subprocess
+from collections import Counter, deque
+
+from sequana.lazy import pandas as pd
+from sequana.lazy import numpy as np
+from sequana.lazy import pylab
+
+from sequana.sequence import Sequence
+from sequana import logger
+logger.name = __name__
+
+__all__ = ["Protein"]
+
+from sequana.iuapc import amino_acids
+aa_letters = list(amino_acids.keys())
+
+from sequana.iuapc import exotic_amino_acids
+exotic_aa_letters = list(exotic_amino_acids.keys())
+
+class Protein(Sequence):
+    """Simple Protein class
+
+        >>> d = Protein("MALWMRLLPLLALLALWGPD")
+        >>> d.check()
+        >>> d.stats()
+
+    """
+    def __init__(self, sequence, use_exotic_amino_acid=True):
+        # make a copy of aa_letters list since we then add elements to it
+        super(Protein, self).__init__(sequence, letters=aa_letters[:])
+        if use_exotic_amino_acid:
+            self._letters += exotic_aa_letters
+        
+
