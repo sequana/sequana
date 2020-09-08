736be2473ddfd895d164844f280354dce799c3cd
Thomas Cokelaer <cokelaer@gmail.com>
2020-09-08 19:08:47 +0200

    add amino acid code

--- sequana/iuapc.py
+++ sequana/iuapc.py
@@ -1,6 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+#  This file is part of Sequana software
+#
+#  Copyright (c) 2016 - Sequana Development Team
+#
+#  File author(s):
+#      Thomas Cokelaer <thomas.cokelaer@pasteur.fr>
+#
+#  Distributed under the terms of the 3-clause BSD license.
+#  The full license is in the LICENSE file, distributed with this software.
+#
+#  website: https://github.com/sequana/sequana
+#  documentation: http://sequana.readthedocs.io
+#
+##############################################################################
+
+
 from sequana import logger
 logger.name = __name__
 
+
+"""
+There are 20 amino acids (Stored in :ref:`amino_acids`) such as alanine.
+Each of them has a name, a 3-letter codes and a one-letter codes (the IUAPC
+code, after the International Union of Pure and Applied chemistry committee).
+
+`ref:`amino_acids is a dictionary with keys as the one-letter code. The values
+are tuples with the 3-letter code and full name.
+
+amino_acids['A'] returns ('Ala', 'Alanine')
+
+Som a protein can be written as a succession of letters made of the keys of the
+dictionary. It is then easy to check the validity of a protein sequence.
+
+
+
+"""
 
 # DNA bases
 dna_bases = ("A", "C", "T", "G")
@@ -63,8 +98,6 @@ dna_complement = {
     'X': 'X',
     'Y': 'R'}
 
-
-
 codons = {
     "UUU":"F", "UUC":"F","UUA":"L", "UUG":"L",
     "CUU":"L", "CUC":"L","CUA":"L", "CUG":"L",
@@ -82,5 +115,52 @@ codons = {
     "CGU":"R", "CGC":"R","CGA":"R", "CGG":"R",
     "AGU":"S", "AGC":"S","AGA":"R", "AGG":"R",
     "GGU":"G", "GGC":"G","GGA":"G", "GGG":"G",
-    }   
+    }
+
+
+amino_acids = {
+"A": ('Ala', 'Alanine'),
+"R": ('Arg', 'Arginine'),
+"N": ('Asn', 'Asparagine'),
+"D": ('Asp', 'Aspartic acid'),
+"C": ('Cys', 'Cysteine'),
+"Q": ('Gln', 'Glutamine'),
+"E": ('Glu', 'Glutamic acid'),
+"G": ('Gly', 'Glycine'),
+"H": ('His', 'Histidine'),
+"I": ('lle', 'Isoleucine'),
+"L": ('Leu', 'Leucine'),
+"K": ('Lys', 'Lysine'),
+"M": ('Met', 'Methionine'),
+"F": ('Phe', 'Pheline'),
+"P": ('Pro', 'Proline'),
+"S": ('Ser', 'Serine'),
+"T": ('Thr', 'Threonine'),
+"W": ('Trp', 'Tryptophan'),
+"Y": ('Tyr', 'Tyrosine'),
+"V": ('Val', 'Valine')
+}
+
+# B and Z codes indicated ambiguous amino acd
+# Pyl and Sec are specified by the UAG  (Pyl) and UGA (Sec) stop codons in a
+# specific context
+exotic_amino_acids = {
+"B": ("Asn or Asp", "Asparagine or aspartic acis"),
+"J": ("Xle", "Isoleucine or leucine"),
+"O": ("Pyl", "Pyrrolysine"),
+"U": ("Sec", "Selenocysteine"),
+"Z": ("Gln or Glu", "Glutamine or glutamic acid"),
+"X": ("Xaa", "Any residue"),
+"--": ("gap", "gap"),
+
+
+}
+
+
+
+
+
+
+
+
 
