050686af5d1197fdae3c47e2914925de230ae6bd
Thomas Cokelaer <cokelaer@gmail.com>
2020-09-08 23:13:55 +0200

    remove sequana_gtf_fixer now in main sequana application

--- test/scripts/test_sequana.py
+++ test/scripts/test_sequana.py
@@ -15,8 +15,8 @@ def test_sequana_app():
     results = runner.invoke(script.fastq, [filename1, '--count-reads'])
     assert results.exit_code == 0
     with TempFile() as fout:
-        results = runner.invoke(script.fastq, [filename1, '--head', 100, '--output',
-fout.name])
+        results = runner.invoke(script.fastq, [filename1, 
+            '--head', 100, '--output', fout.name])
         assert results.exit_code == 0
 
     # The samplesheet command
@@ -27,6 +27,14 @@ fout.name])
     results = runner.invoke(script.samplesheet, [filename1, '--check'])
     results = runner.invoke(script.samplesheet, [filename2, '--check'])
     results = runner.invoke(script.samplesheet, [filename2, '--extract-adapters'])
+
+    # gtf fixer
+    filename = sequana_data("test_gtf_fixer.gtf")
+    with TempFile() as fout:
+        results = runner.invoke(script.gtf_fixer, 
+            ['--input', filename, "--output", fout.name])
+    
+
 
 def test_sequana_app_user():
     filename1 = sequana_data("test.fastq")
@@ -42,6 +50,8 @@ def test_sequana_app_user():
     cmd = ["sequana", "fastq", filename1, "--count-reads"]
     subprocess.call(cmd)
     
+
+
 
     #with TempFile() as fout:
     #    cmd = ["sequana", "fastq", filename1, "--head", '--output', fout.name]
