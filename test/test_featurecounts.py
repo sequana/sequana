d08a263be27708ce7907cae4c19bee7887f902f5
Thomas Cokelaer <cokelaer@gmail.com>
2020-11-17 14:36:21 +0100

    fix test

--- test/test_featurecounts.py
+++ test/test_featurecounts.py
@@ -9,18 +9,19 @@ def test_featurecounts():
     RNASEQ_DIR_undef = sequana_data("featurecounts") + "/rnaseq_undef"
     RNASEQ_DIR_noconsensus = sequana_data("featurecounts") + "/rnaseq_noconsensus"
 
-    assert fc.get_most_probable_strand_consensus(RNASEQ_DIR_0) == "0"
-    assert fc.get_most_probable_strand_consensus(RNASEQ_DIR_1) == "1"
-    assert fc.get_most_probable_strand_consensus(RNASEQ_DIR_2) == "2"
+    assert fc.get_most_probable_strand_consensus(RNASEQ_DIR_0, tolerance=0.1) == "0"
+    assert fc.get_most_probable_strand_consensus(RNASEQ_DIR_1, tolerance=0.1) == "1"
+    assert fc.get_most_probable_strand_consensus(RNASEQ_DIR_2, tolerance=0.1) == "2"
 
     try:
-        fc.get_most_probable_strand_consensus(RNASEQ_DIR_undef)
+        fc.get_most_probable_strand_consensus(RNASEQ_DIR_undef, tolerance=0.1)
         assert False
     except IOError:
         assert True
 
     try:
-        fc.get_most_probable_strand_consensus(RNASEQ_DIR_noconsensus)
+        fc.get_most_probable_strand_consensus(RNASEQ_DIR_noconsensus,
+tolerance=0.1)
         assert False
     except IOError:
         assert True
