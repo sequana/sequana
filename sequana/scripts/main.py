3de1e45d35a65c92d8a1face464c665635a939d8
Thomas Cokelaer <cokelaer@gmail.com>
2020-11-17 14:35:46 +0100

    implement the merge option in sequana CLI

--- sequana/scripts/main.py
+++ sequana/scripts/main.py
@@ -55,12 +55,14 @@ def main(**kwargs):
 
 
 @main.command()
-@click.argument('filename', type=click.STRING)
-@click.option("--count-reads", is_flag=True)
+@click.argument('filename', type=click.STRING, nargs=-1)
 @click.option("-o", "--output",
     help="filename where to save results. to be used with --head, --tail")
+
+@click.option("--count-reads", is_flag=True)
 @click.option("--head", type=click.INT,
     help='number of reads to extract from the head')
+@click.option("--merge", is_flag=True)
 @click.option("--tail", type=click.INT,
     help="number of reads to extract from the tail")
 def fastq(**kwargs):
@@ -71,21 +73,47 @@ def fastq(**kwargs):
     """
     from sequana.fastq import FastQ
 
+    filenames = kwargs['filename']
+    # users may provide a wildcards such as "A*gz" or list of files.
+    if len(filenames) == 1:
+        # if existing files or glob, a glob would give the same answer.
+        filenames = glob.glob(filenames[0])
+    for filename in filenames:
+        os.path.exists(filename)
+
     # could be simplified calling count_reads only once
     if kwargs['count_reads']:
-        for filename in glob.glob(kwargs["filename"]):
+        for filename in filenames:
             f = FastQ(filename)
             Nreads = f.count_reads()
             Nlines = Nreads * 4
             print(f"Number of reads in {filename}: {Nreads}")
             print(f"Number of lines in {filename}: {Nlines}")
     elif kwargs['head']:
-        f = FastQ(kwargs['filename'])
-        assert kwargs['output']
-        N = kwargs['head'] * 4
-        f.extract_head(N=N, output_filename=kwargs['output'])
+        for filename in filenames:
+            f = FastQ(filename)
+            assert kwargs['output']
+            N = kwargs['head'] * 4
+            f.extract_head(N=N, output_filename=kwargs['output'])
     elif kwargs['tail']: #pragma: no cover
         raise NotImplementedError
+    elif kwargs['merge']:
+        import subprocess
+        # merge all input files (assuming gz extension)
+        extensions = [filename.split(".")[-1] for filename in filenames]
+        if set(extensions) != set(['gz']):
+            raise ValueError("Your input FastQ files must be zipped")
+        output_filename = kwargs['output']
+        if output_filename is None:
+            logger.error("You must use --output filename.gz")
+            sys.exit(1)
+        if output_filename.endswith(".gz") is False:
+            raise ValueError("your output file must end in .gz")
+
+        p1 = subprocess.Popen(['zcat'] +  list(filenames),  stdout=subprocess.PIPE)
+        fout = open(output_filename, 'wb')
+        p2 = subprocess.run(['pigz'], stdin=p1.stdout, stdout=fout)
+
     else:  #pragma: no cover
         print("Use one of the commands")
 
