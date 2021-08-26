4eb7e6b3c1fb0e230311921f1ccd21c801b62632
Thomas Cokelaer <cokelaer@gmail.com>
2021-08-26 16:52:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import setup, find_packages
 import glob
 
 _MAJOR               = 0
-_MINOR               = 11
-_MICRO               = 1
+_MINOR               = 12
+_MICRO               = 0
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
 
