f12beb5d2fbdc72c12f473c5cac04716f4893666
Etienne Kornobis <ekornobis@gmail.com>
2021-09-09 16:49:24 +0200

    Update test for volcano plot

--- test/viz/test_volcano.py
+++ test/viz/test_volcano.py
@@ -1,11 +1,13 @@
 from sequana.viz import Volcano
+import pandas as pd
+
 
 def test1():
     import numpy as np
+
     fc = np.random.randn(1000)
     pvalue = np.random.randn(1000)
-    v = Volcano(fc, -np.log10(pvalue**2), pvalue_threshold=3)
+    df = pd.DataFrame({"log2FoldChange": fc, "padj": pvalue ** 2})
+    v = Volcano(data=df, pvalue_threshold=3)
     v.plot()
     v.plot(logy=True)
-
-
