e10e5f55fe41ab5b0c2c9fda0f98da306b2ceb36
Thomas Cokelaer <cokelaer@gmail.com>
2021-09-29 14:58:17 +0200

    move files
    Update test and add test for the new feature counts layout
    
    update test
    
    Clean test_fastq test and resources

--- test/test_featurecounts.py
+++ test/test_featurecounts.py
@@ -1,28 +1,36 @@
+import os
+import glob
+
 import sequana.featurecounts as fc
 from sequana import sequana_data
 
+from . import test_dir
+test_dir = f"{test_dir}/data"
+
+RNASEQ_DIR_0 = f"{test_dir}/featurecounts/rnaseq_0"
+RNASEQ_DIR_1 = f"{test_dir}/featurecounts/rnaseq_1"
+RNASEQ_DIR_2 = f"{test_dir}/featurecounts/rnaseq_2"
+RNASEQ_DIR_undef = f"{test_dir}/featurecounts/rnaseq_undef"
+RNASEQ_DIR_noconsensus = f"{test_dir}/featurecounts/rnaseq_noconsensus"
+RNASEQ_DIR = f"{test_dir}/featurecounts/featurecounts_ex1"
+RNASEQ_NEW_1= f"{test_dir}/featurecounts/new_rnaseq_output"
+
 
 def __test_file():
-    RNASEQ_DIR_0 = sequana_data("featurecounts") + "/rnaseq_0"
-    import os
+    RNASEQ_DIR_0 = f"{test_dir}/featurecounts/rnaseq_0"
     assert os.path.isdir(RNASEQ_DIR_0)
     assert os.path.isdir(RNASEQ_DIR_0+"/sample1")
     assert os.path.isdir(RNASEQ_DIR_0+"/sample1/feature_counts_0")
-    import glob
     print(glob.glob(RNASEQ_DIR_0+"/sample1/feature_counts_0/*"))
 
 def test_featurecounts():
-    RNASEQ_DIR_0 = sequana_data("featurecounts") + "/rnaseq_0"
-    RNASEQ_DIR_1 = sequana_data("featurecounts") + "/rnaseq_1"
-    RNASEQ_DIR_2 = sequana_data("featurecounts") + "/rnaseq_2"
-    RNASEQ_DIR_undef = sequana_data("featurecounts") + "/rnaseq_undef"
-    RNASEQ_DIR_noconsensus = sequana_data("featurecounts") + "/rnaseq_noconsensus"
 
     print( fc.get_most_probable_strand_consensus(RNASEQ_DIR_0, tolerance=0.1))
 
     assert fc.get_most_probable_strand_consensus(RNASEQ_DIR_0, tolerance=0.1)[0] == 0
     assert fc.get_most_probable_strand_consensus(RNASEQ_DIR_1, tolerance=0.1)[0] == 1
     assert fc.get_most_probable_strand_consensus(RNASEQ_DIR_2, tolerance=0.1)[0] == 2
+    assert fc.get_most_probable_strand_consensus(RNASEQ_NEW_1, tolerance=0.1)[0] == 1
 
     assert (
         fc.get_most_probable_strand_consensus(RNASEQ_DIR_undef, tolerance=0.1)[0] == -1
@@ -37,22 +45,12 @@ def test_featurecounts():
     #    assert True
 
 def test_multi_feature_counts():
-    RNASEQ_DIR_0 = sequana_data("featurecounts") + "/rnaseq_0"
     ff = fc.MultiFeatureCount(RNASEQ_DIR_0, 0.15)
     ff.get_most_probable_strand_consensus()
     ff.plot_strandness()
 
-import os
-import pytest
-skiptravis = pytest.mark.skipif("TRAVIS_PYTHON_VERSION" in os.environ and 
-    os.environ["TRAVIS_PYTHON_VERSION"].startswith("3.6"),
-    reason="Py3.6 On travis")
-
-@skiptravis
 def test_feature_counts():
-    RNASEQ_DIR = sequana_data("featurecounts/featurecounts_ex1") 
     fc1 = fc.FeatureCount(RNASEQ_DIR + "/all_features.out")
-    import glob
     fc2 = fc.FeatureCount(glob.glob(RNASEQ_DIR + "/*feature.out"))
 
     # we sort index to avoid error in python 3.6 travis
