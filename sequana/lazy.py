6468c8325eed45d1bfbf0166bf0ebb5e86a53888
Thomas Cokelaer <cokelaer@gmail.com>
2021-10-19 15:10:42 +0200

    remove lazy import (useless)

--- sequana/lazy.py
+++ sequana/lazy.py
@@ -24,7 +24,6 @@ numpy = LazyImport('numpy')
 scipy = LazyImport('scipy')
 scipy_stats = LazyImport('scipy.stats')
 pandas = LazyImport('pandas')
-vcf = LazyImport("vcfpy")
 
 def enabled():
     "Returns ``True`` if LazyImports are globally enabled"
