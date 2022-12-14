6973fb9f710f87dbf667a2897debdd270312039c
Thomas Cokelaer <cokelaer@gmail.com>
2022-12-14 18:04:52 +0100

    Add new modules in relation with chipseq pipelines

--- sequana/macs3.py
+++ sequana/macs3.py
@@ -10,41 +10,33 @@
 #  documentation: http://sequana.readthedocs.io
 #
 ##############################################################################
-import itertools
 
-from sequana.lazy import pandas as pd
 from sequana.lazy import pylab
-from sequana.lazy import numpy as np
+from sequana.lazy import pandas as pd
+from sequana.utils.pandas import PandasReader
+from sequana.viz.venn import plot_venn
 
 import colorlog
 
 logger = colorlog.getLogger(__name__)
 
 
-class PandasReader:
-    def __init__(self, filename, sep="\t", **kwargs):
-        self.df = pd.read_csv(filename, sep=sep, **kwargs)
-        # If there is a header, let us strip it from spaces
-        try:
-            self.df.columns = [x.strip() for x in self.df.columns]
-        except:
-            pass
-
-        # let us strip strings from spaces
-        for x in self.df.columns:
-            try:
-                self.df[x] = self.df[x].str.strip()
-            except Exception:
-                pass
+__all__ = ["MACS3Reader", "PeakConsensus"]
 
 
-class MACS3Reader(PandasReader):
+class MACS3Reader:
     """This class reads output of macs3 tool
 
+    ::
 
-    If input file ends in .xls, we assume this is a NAME_peaks.xls file:
-    a tabular file which contains information about called peaks. The columns
-    are then filled using the header that should be:
+        from sequana import MACS3Reader
+        mr = MACS3Reader(data)
+        mr.df
+        mr.plot_volcano()
+
+
+    If input file ends in .xls, we assume this is a NAME_peaks.xls file. It is
+    therefore a tabulated file where columns are:
 
     - chromosome name
     - start position of peak
@@ -66,14 +58,18 @@ class MACS3Reader(PandasReader):
 
     The fifth column is the **score** calculate as int(-10*log10_pvalue) or
     int(-10*log10_pvalue) where log10-pvalue/log10-qvalue is the 8th or 9th column
-    depending on whether -p (pvalue) or -q (qvalue) is used as score cutoff.
-    The 7th columns stores the fold-change at peak summit
-    8th: -log10pvalue at peak summit
-    9th: -log10qvalue at peak summit
-    10th: relative summit position to peak start equivalent to absolute peak
-    summit position in the xls file
+    depending on whether -p (pvalue) or -q (qvalue) is used as score cutoff. The 7th
+    columns stores the fold-change at peak summit. The 8th is -log10pvalue at peak summit
+    and the 9th is -log10qvalue at peak summit. The 10th is the relative summit position
+    to peak start equivalent to absolute peak summit position in the xls file.
 
-    NAME_peaks.broadPeak is in BED6+3 format which is similar to narrowPeak file, except for missing the 10th column for annotating peak summits. This file and the gappedPeak file will only be available when --broad is enabled. Since in the broad peak calling mode, the peak summit won't be called, the values in the 5th, and 7-9th columns are the mean value across all positions in the peak region. Refer to narrowPeak if you want to fix the value issue in the 5th column
+    NAME_peaks.broadPeak is in BED6+3 format which is similar to narrowPeak file, except
+    for missing the 10th column for annotating peak summits. This file and the
+    gappedPeak file will only be available when --broad is enabled. Since in the
+    broad peak calling mode, the peak summit won't be called, the values in the 5th,
+    and 7-9th columns are the mean value across all positions in the peak region.
+    Refer to narrowPeak if you want to fix the value issue in the 5th column
+
     .. reference:: https://github.com/macs3-project/MACS/blob/master/docs/callpeak.md
     """
 
@@ -82,7 +78,8 @@ class MACS3Reader(PandasReader):
         # .xls is not stuctured similarly to .narrowPeak
         if filename.endswith(".xls"):
             #
-            self.df = pd.read_csv(filename, comment="#", sep="\t")
+            self.df = PandasReader(filename, comment="#", sep="\t").df
+            # narrow and broad files have this header in common and can be checked.
             for x in [
                 "chr",
                 "start",
@@ -93,12 +90,11 @@ class MACS3Reader(PandasReader):
                 "-log10(qvalue)",
                 "name",
             ]:
-                # we could have also pileup depeding on the user's options
+                # we could have also pileup depending on the user's options
                 assert x in self.df.columns
             self.df["stop"] = self.df["end"]
         elif filename.endswith("narrowPeak"):
-            self.df = pd.read_csv(filename, header=None, sep="\t")
-            self.df.columns = [
+            columns = [
                 "chr",
                 "start",
                 "stop",
@@ -110,10 +106,9 @@ class MACS3Reader(PandasReader):
                 "-log10(qvalue)",
                 "abs_summit_from_start",
             ]
-            self.df["end"] = self.df["stop"]
+            self.df = PandasReader(filename, header=None, sep="\t", columns=columns).df
         elif filename.endswith("broadPeak"):
-            self.df = pd.read_csv(filename, header=None, sep="\t")
-            self.df.columns = [
+            columns = [
                 "chr",
                 "start",
                 "stop",
@@ -124,14 +119,15 @@ class MACS3Reader(PandasReader):
                 "-log10(pvalue)",
                 "-log10(qvalue)",
             ]
-            self.df["end"] = self.df["stop"]
+            self.df = PandasReader(filename, header=None, sep="\t", columns=columns).df
         self.df["length"] = self.df["stop"] - self.df["start"]
         #
 
     def __len__(self):
         return len(self.df)
 
-    def plot_volcano(self, plotly=False, marker_color="b"):
+    def plot_volcano(self, plotly=False, marker_color="b", title=""):
+        """Plots log10 versus fold change of all peaks"""
 
         if plotly:
             from plotly import express as px
@@ -139,13 +135,7 @@ class MACS3Reader(PandasReader):
             df = self.df.copy()
             df["log_adj_pvalue"] = self.df["-log10(pvalue)"]
             df["log2FoldChange"] = pylab.log2(self.df["fold_enrichment"])
-            # df['hover_name'] = df['start']
-            hover_name = "start"
-
-            try:
-                df["info"] = df["chr"] + ":" + df["start"].astype(str) + "-" + df["stop"].astype(str)
-            except KeyError:
-                df["info"] = df["chr"] + ":" + df["start"].astype(str) + "-" + df["end"].astype(str)
+            df["info"] = df["chr"] + ":" + df["start"].astype(str) + "-" + df["stop"].astype(str)
 
             fig = px.scatter(
                 df,
@@ -155,18 +145,9 @@ class MACS3Reader(PandasReader):
                 log_y=False,
                 color="length",
                 height=600,
+                title=title,
                 labels={"log2_fold_enrichment": "log10 p-value"},
             )
-            """fig.update_layout(
-                 shapes=[dict(type='line', 
-                               xref='x', x0=df.log2FoldChange.min(), x1=df.log2FoldChange.max(), 
-                               yref='y', y0=-pylab.log10(padj), y1=-pylab.log10(padj), 
-                             line=dict(
-                                  color="black",
-                                  width=1,
-                                  dash="dash"))
-                        ])
-            """
             return fig
 
         pylab.scatter(
@@ -180,3 +161,96 @@ class MACS3Reader(PandasReader):
         )
         pylab.xlabel("Fold enrichment")
         pylab.ylabel("log10 pvalue")
+        pylab.title(title)
+
+
+class PeakConsensus:
+    def __init__(self, f1, f2):
+        self.df1 = MACS3Reader(f1).df
+        self.df1["category"] = "first"
+        self.df2 = MACS3Reader(f2).df
+        self.df2["category"] = "second"
+        self.df_merged = self.merge()
+
+    def merge(self, overlap=0.2):
+        df = pd.concat([self.df1, self.df2]).sort_values(["chr", "start"])
+        # if overlap at least one base, we merge the peaks and label them with
+        # common information, otherwise we report the original peak
+
+        merged = []
+        prev = None
+        N1 = 0
+        N2 = 0
+        N12 = 0
+        skip_next = True
+        for k, current in df.iterrows():
+            if skip_next:
+                prev = current
+                skip_next = False
+                continue
+
+            # if current overlaps the prev start or end, there is overlap
+            # or if current included in prev there current and prev overlaps
+            if current["start"] <= prev["start"] and current["stop"] >= prev["start"]:
+                overlap = True
+                N12 += 1
+            elif current["start"] <= prev["stop"] and current["stop"] >= prev["stop"]:
+                overlap = True
+                N12 += 1
+            elif current["start"] >= prev["start"] and current["stop"] <= prev["stop"]:
+                overlap = True
+                N12 += 1
+            else:
+                overlap = False
+                if prev["name"].startswith("1_vs_6_7"):
+                    N1 += 1
+                elif prev["name"].startswith("2_vs_6_7"):
+                    N2 += 1
+
+            if overlap:
+                m = min(current["start"], prev["start"])
+                M = max(current["stop"], prev["stop"])
+                data = current.copy()
+                data["start"] = m
+                data["stop"] = M
+                data["category"] = "both"
+                merged.append(data)
+                skip_next = True
+            else:
+                m = min(current["start"], prev["start"])
+                M = max(current["stop"], prev["stop"])
+                merged.append(prev)
+                skip_next = False
+
+            prev = current
+        df = pd.DataFrame(merged)
+        df = df.reset_index(drop=True)
+        return df
+
+    def plot_venn(self, title="", labels=[]):
+
+        plot_venn(
+            (
+                set(self.df_merged.query("category in ['first', 'both']").index),
+                set(self.df_merged.query("category in ['second', 'both']").index),
+            ),
+            labels=(("first", "second")),
+        )
+
+    def to_saf(self, filename):
+        df = self.df_merged.reset_index()
+        df["strand"] = "+"
+        df["GeneID"] = df["index"]
+        df[["GeneID", "chr", "start", "stop", "strand", "category"]].to_csv(filename, sep="\t", index=None)
+
+    def to_bed(self, filename):
+        # output to be used by homer
+        # annotatePeaks.pl file.bed file.fa -gid -gff file.gff
+        # GeneID seems to have to start with Interval_ ??
+
+        df = self.df_merged.reset_index()
+        df["strand"] = "+"
+        df["GeneID"] = ["Interval_{}".format(x) for x in df["index"]]
+        mapper = {"both": 3, "first": 1, "second": 2}
+        df["score"] = [mapper[x] for x in df["category"]]
+        df[["chr", "start", "stop", "GeneID", "score", "strand"]].to_csv(filename, sep="\t", index=None, header=False)
---
+++ test/test_idr.py
@@ -0,0 +1,24 @@
+import pytest
+
+from sequana.idr import IDR
+
+from . import test_dir
+
+def test_idr(tmpdir):
+
+    fout = tmpdir.join('test.png')
+
+    for mode in ['narrow', 'broad']:
+        idr = IDR(f"{test_dir}/data/IDR/{mode}.csv")
+        assert idr.mode == mode
+        len(idr)
+        idr.plot_ranks(fout, savefig=True)
+        idr.plot_scores(fout, savefig=True)
+        idr.plot_rank_vs_idr_score(fout, savefig=True)
+        idr.plot_idr_vs_peaks(fout, savefig=True)
+
+        assert idr.IDR2score(0) == 1000
+        assert idr.IDR2score(0.05) == 540
+        assert idr.IDR2score(1) == 0
+        assert idr.score2IDR(0) == 1
+        assert pytest.approx(idr.score2IDR(540), 0.01) == 0.05
