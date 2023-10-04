fde1af7f342cfca2d844766ce250ea22d1f2f8f3
Etienne Kornobis <ekornobis@gmail.com>
2023-10-04 15:21:32 +0200

    Fix RNADiffResults annotation and kegg save pathways simplifcation

--- test/enrichment/test_kegg.py
+++ test/enrichment/test_kegg.py
@@ -6,35 +6,32 @@ import glob
 from . import test_dir
 
 
-
 def test_ke(tmpdir):
     up = pd.read_csv(f"{test_dir}/data/ecoli_up_gene.csv")
     down = pd.read_csv(f"{test_dir}/data/ecoli_down_gene.csv")
     up = list(up.Name)
     down = list(down.Name)
-    gene_lists = {'up': up, 'down': down, 'all': up +down}
+    gene_lists = {"up": up, "down": down, "all": up + down}
 
     from sequana import logger
-    logger.setLevel('INFO')
-    ke = KEGGPathwayEnrichment(gene_lists, "eco",
-            preload_directory=f"{test_dir}/data/kegg_pathways/")
+
+    logger.setLevel("INFO")
+    ke = KEGGPathwayEnrichment(gene_lists, "eco", preload_directory=f"{test_dir}/data/kegg_pathways/")
 
     with pytest.raises(ValueError):
-        ke.barplot('dummy')
+        ke.barplot("dummy")
 
-    ke.barplot('up')
-    ke.barplot('down')
+    ke.barplot("up")
+    ke.barplot("down")
     ke.plot_genesets_hist()
-    ke.scatterplot('down')
+    ke.scatterplot("down")
     assert ke.find_pathways_by_gene("moaA")
 
-
     # save one pathway (just one to speed up things)
-    outpng = tmpdir.join('test.png')
+    outpng = tmpdir.join("test.png")
     df = pd.read_csv(f"{test_dir}/data/ecoli_all_gene.csv", index_col=0)
-    ke.save_pathway('eco04122', df, filename=outpng)
+    ke.save_pathway("eco04122", df, filename=outpng)
 
     # save all pathways (same as input)
     path = tmpdir.mkdir("pathways_tmp")
-    ke.export_pathways_to_json(str(path))
-
+    ke.save_pathways(str(path))
