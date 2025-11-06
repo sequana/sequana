cd362943e04bbc44dd5d043d091fd376d2b5c0e6
Thomas Cokelaer <cokelaer@gmail.com>
2025-11-06 13:39:09 +0100

    add tests

--- test/test_trf.py
+++ test/test_trf.py
@@ -7,4 +7,4 @@ def test_trf():
     tt = TRF(f"{test_dir}/data/trf/test_trf1.dat")
     tt.hist_period_size()
     tt.hist_entropy()
-    tt.hist_repet_by_sequence()
+    tt.hist_repetitions_per_sequence()
