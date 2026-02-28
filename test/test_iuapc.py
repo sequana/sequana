13bd0221f95b7a52dee3a05d9d8892dbd8598f1e
Thomas Cokelaer <cokelaer@gmail.com>
2026-02-28 23:14:08 +0100

    add new dictionary and update test

--- test/test_iuapc.py
+++ test/test_iuapc.py
@@ -4,5 +4,6 @@ from sequana.iuapc import *
 def test_all():
     assert dna_bases == ("A", "C", "T", "G")
     assert dna_ambiguities["B"] == "[CGT]"
+    assert dna_ambiguities_r["AG"] == "R"
     assert dna_complement["A"] == "T"
     assert codons["AUC"] == "I"
